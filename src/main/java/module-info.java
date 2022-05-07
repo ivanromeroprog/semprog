bae39ff8464e87f7f0c57baa89d1dd0bece7c5ad
Iván Yamil Romero <31389316+knewlife@users.noreply.github.com>
2022-05-06 22:28:58 -0300

    primer test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.seminarioprogramacion.semtest {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.seminarioprogramacion.semtest to javafx.fxml;
+    exports com.seminarioprogramacion.semtest;
+}
+*/
