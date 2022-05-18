6e7629746ee8bc785ef3b478d19c85857f578c74
Iván Yamil Romero <31389316+knewlife@users.noreply.github.com>
2022-05-18 19:35:37 -0300

    Agregado mecanico al menu principal

--- src/main/java/com/seminarioprogramacion/semtest/MecanicosController.java
+++ src/main/java/com/seminarioprogramacion/semtest/MecanicosController.java
@@ -6,16 +6,32 @@ package com.seminarioprogramacion.semtest;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 /**
  *
  * @author Win10
  */
 public class MecanicosController {
+
+    @FXML
+    private Button btningreso1;
+    @FXML
+    private Button btningreso2;
+    @FXML
+    private Button btningreso;
+    @FXML
+    private Button btningreso3;
     
 
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     } 
+
+    @FXML
+    private void switchToMain(ActionEvent event) {
+    }
     
 }
