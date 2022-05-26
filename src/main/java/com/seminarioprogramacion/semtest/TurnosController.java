c5aebef3142f3b9e860953df9ceadd707a00784b
Iván Yamil Romero <31389316+knewlife@users.noreply.github.com>
2022-05-26 15:20:26 -0300

    mdoficaciones en menu principal

--- src/main/java/com/seminarioprogramacion/semtest/TurnosController.java
+++ src/main/java/com/seminarioprogramacion/semtest/TurnosController.java
@@ -7,9 +7,12 @@ package com.seminarioprogramacion.semtest;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 /**
@@ -46,6 +49,21 @@ public class TurnosController implements Initializable {
         //((Stage) menup.getScene().getWindow()).close();
         
         App.newWindow("AsignarTurno",((Stage) btnasignarturno.getScene().getWindow()),"Asignar Turno");
+    }
+    
+        @FXML
+    private void EliminarTurno(ActionEvent event) {
+        String mensaje = "¿Está seguro que quiere eliminar este Turno?";
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, mensaje, ButtonType.YES, ButtonType.NO);
+        alert.showAndWait();
+    }
+    
+        
+        @FXML
+    private void ConfirmarAsistencia(ActionEvent event) {
+        String mensaje = "¿El titular asistió al turno seleccionado?";
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, mensaje, ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+        alert.showAndWait();
     }
     
 }
